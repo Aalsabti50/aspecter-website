c4322af9653e84a5384067f7d3ee91a6b502b629
Aalsabti50 <102615853+Aalsabti50@users.noreply.github.com>
2022-05-07 17:01:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,18 +35,12 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Graphic Designer", "Human"],
+        strings: ["Developer", "Graphic Designer", "Human"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
